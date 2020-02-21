9d7a5b7188f38a9beb596ac10de0f6e413d12577
3kesyuri <1989sintetsu@gmail.com>
2020-02-21 17:11:37 +0900

    画像投稿と表示機能をテストに実装

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,18 @@
 class TestsController < ApplicationController
 
   def index
+    @test = Test.new
+    @test_obj = Test.all
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save!
+      redirect_to root_path
+    end
+  end
+
+  def test_params
+    params.require(:test).permit(:text, :image)
   end
 end
