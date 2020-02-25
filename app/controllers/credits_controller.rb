6d644e7f903a5922dfc1bbc94f56a262a5671dd2
3kesyuri <1989sintetsu@gmail.com>
2020-02-25 20:19:59 +0900

    activehash1導入　&　destination作成

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,2 +1,8 @@
 class CreditsController < ApplicationController
+
+  def new
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tests#index"
   resources :tests, only: [:index, :create]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :credits, only: [:index,:new]
+    resources :destinations, only: [:index]
+  end
 end
