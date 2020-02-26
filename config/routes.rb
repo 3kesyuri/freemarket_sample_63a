f0b80d58adc0baff93f80637046e7beeb5bdbca9
uko-h <hourglassu@yahoo.co.jp>
2020-02-26 21:15:31 +0900

    user新規登録とログインのHTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "tests#index"
+  root "users#index"
   resources :tests, only: [:index, :create]
+  resources :users, only: [:index]
 end
