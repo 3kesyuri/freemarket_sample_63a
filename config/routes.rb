b47813ed69b5837a475b9c5868e761693f1f8039
3kesyuri <1989sintetsu@gmail.com>
2020-03-09 20:50:45 +0900

    購入確認ページ完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   root "products#index"
   resources :tests, only: [:index, :create]
   resources :destinations, only: [:create, :update]
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create] do
+    resources :purchases, only: [:index],shallow: true do
+      collection do
+        get 'index', to: 'purchases#index'
+        post 'pay', to: 'purchases#pay'
+        get 'done', to: 'purchases#done'
+      end
+    end
+  end
+
+
   resources :users, only: [:show, :edit, :update] do
     resources :destinations, only: [:index]
     resources :credits, only: [:new, :show],shallow: true do
@@ -14,4 +24,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  
+
 end
