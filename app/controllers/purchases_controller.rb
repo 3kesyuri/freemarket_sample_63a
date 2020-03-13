ad00c40a7d94c589df3d806d01ae45352c4dadaa
3kesyuri <1989sintetsu@gmail.com>
2020-03-13 21:21:11 +0900

    インデント修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -17,10 +17,10 @@ class PurchasesController < ApplicationController
     set_purchase_amaout
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => @amount, 
-    :customer => @credit.name, 
-    :currency => 'jpy', 
-  )
+      :amount => @amount, 
+      :customer => @credit.name, 
+      :currency => 'jpy', 
+    )
 
     set_product_id
     product = Product.find_by(@product_id)
