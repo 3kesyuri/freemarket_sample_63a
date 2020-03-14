b1a6222dc4251cbe750c89f764786c44c47c600c
uko-h <hourglassu@yahoo.co.jp>
2020-03-14 16:43:30 +0900

    コントローラーの変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,11 +8,23 @@ class ProductsController < ApplicationController
   def new
     authenticate_user!
     @product = Product.new
-    render layout: 'application'
+    @product.product_images.new
+
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save! 
+      redirect_to root_path
+    else
+      redirect_to new_product_path
+    end
   end
 
+  private
 
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :brand_id, :condition, :delivery_charge, :delivery_origin, :shipping_date, :price, product_images_attributes: [:image]).merge(user_id: current_user.id)
+  end
 end
+
