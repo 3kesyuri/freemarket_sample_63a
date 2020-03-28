d0d41f8bf65818a35579b300e6a3ba16b63e1d1f
3kesyuri <1989sintetsu@gmail.com>
2020-03-28 15:27:49 +0900

    JSのコメントを削除

--- app/assets/javascripts/ products.js
+++ app/assets/javascripts/ products.js
@@ -15,76 +15,69 @@ $(document).on('turbolinks:load', function(){
       return html;
     }
 
-    // ラベルのwidth操作
     function setLabel() {
-      //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
+
       var prevContent = $('.content__product--image-label').prev();
       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
       $('.content__product--image-label').css('width', labelWidth);
     }
 
-    // プレビューの追加
+
     $(document).on('change', '.hidden-field', function() {
       setLabel();
-      //hidden-fieldのidの数値のみ取得
+
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //labelボックスのidとforを更新
+
       $('.label-box').attr({id: `label-box--${id}`,for: `product_product_images_attributes_${id}_image`});
-      //選択したfileのオブジェクトを取得
+
       var file = this.files[0];
       var reader = new FileReader();
-      //readAsDataURLで指定したFileオブジェクトを読み込む
+
       reader.readAsDataURL(file);
-      //読み込み時に発火するイベント
+
       reader.onload = function() {
         var image = this.result;
-        //プレビューが元々なかった場合はhtmlを追加
+
         if ($(`#preview-box__${id}`).length == 0) {
           var count = $('.preview-box').length;
           var html = buildHTML(id);
-          //ラベルの直前のプレビュー群にプレビューを追加
+
           var prevContent = $('.content__product--image-label').prev();
           $(prevContent).append(html);
         }
-        //イメージを追加
+
         $(`#preview-box__${id} img`).attr('src', `${image}`);
         var count = $('.preview-box').length;
-        //プレビューが5個あったらラベルを隠す 
+
         if (count == 5) { 
           $('.content__product--image-label').hide();
         }
 
-        //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=============
+
         if ($(`#product_product_images_attributes_${id}__destroy`)){
           $(`#product_product_images_attributes_${id}__destroy`).prop('checked',false);
         } 
-        //=============================================================================
 
-        //ラベルのwidth操作
         setLabel();
-        //ラベルのidとforの値を変更
+
         if(count < 5){
           $('.label-box').attr({id: `label-box--${count}`,for: `product_product_images_attributes_${count}_image`});
         }
       }
     });
 
-    // 画像の削除
     $(document).on('click', '.delete-box', function() {
       var count = $('.preview-box').length;
       setLabel(count);
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
       $(`#preview-box__${id}`).remove();
 
-      //新規登録時と編集時の場合分け==========================================================
 
-      //新規投稿時
-      //削除用チェックボックスの有無で判定
       if ($(`#product_product_images_attributes_${id}__destroy`).length == 0) {
-        //フォームの中身を削除 
+
         $(`#product_product_images_attributes_${id}_image`).val("");
         var count = $('.preview-box').length;
-        //5個めが消されたらラベルを表示
+
         if (count == 4) {
           $('.content__product--image-label').show();
         }
@@ -95,43 +88,36 @@ $(document).on('turbolinks:load', function(){
         }
       } else {
 
-        //投稿編集時
         $(`#product_product_images_attributes_${id}__destroy`).prop('checked',true);
-        //5個めが消されたらラベルを表示
+
         if (count == 4) {
           $('.content__product--image-label').show();
         }
 
-        //ラベルのwidth操作
         setLabel();
-        //ラベルのidとforの値を変更
-        //削除したプレビューのidによって、ラベルのidを変更する
+
         if(id < 5){
           $('.label-box').attr({id: `label-box--${id}`,for: `product_product_images_attributes_${id}_image`});
         }
       }
-      //=============================================================================
     });
   });
 
-
-      // 投稿編集時
-    //products/:i/editページへリンクした際のアクション=======================================
     if (window.location.href.match(/\/products\/\d+\/edit/)){
-      //登録済み画像のプレビュー表示欄の要素を取得する
+
       var prevContent = $('.content__product--image-label').prev();
       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
       $('.content__product--image-label').css('width', labelWidth);
-      //プレビューにidを追加
+
       $('.preview-box').each(function(index, box){
         $(box).attr('id', `preview-box__${index}`);
       })
-      //削除ボタンにidを追加
+
       $('.delete-box').each(function(index, box){
         $(box).attr('id', `delete_btn_${index}`);
       })
       var count = $('.preview-box').length;
-      //プレビューが5あるときは、投稿ボックスを消しておく
+
       if (count == 5) {
         $('.content__product--image-label').hide();
       }
@@ -159,5 +145,4 @@ $(document).on('turbolinks:load', function(){
         });
       });
     }
-    //=============================================================================
 });
