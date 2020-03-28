e8587fd698a89964cbfa745946eea5105846cba6
uko-h <hourglassu@yahoo.co.jp>
2020-03-29 00:12:26 +0900

    カテゴリーの動的セレクトボックスのJSとコメントアウトの消去

--- app/assets/javascripts/ products.js
+++ app/assets/javascripts/ products.js
@@ -1,7 +1,5 @@
 $(document).on('turbolinks:load', function(){
   $(function(){
-
-    //プレビューのhtmlを定義
     function buildHTML(count) {
       var html = `<div class="preview-box" id="preview-box__${count}">
                     <div class="upper-box">
@@ -19,79 +17,134 @@ $(document).on('turbolinks:load', function(){
       return html;
     }
 
-    // ラベルのwidth操作
     function setLabel() {
-      //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
       var imageContent = $('.content__product--image-label').prev();
       labelWidth = (620 - $(imageContent).css('width').replace(/[^0-9]/g, ''));
       $('.content__product--image-label').css('width', labelWidth);
     }
 
-    // プレビューの追加
     $(document).on('change', '.hidden-field', function() {
       setLabel();
-      //hidden-fieldのidの数値のみ取得
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //labelボックスのidとforを更新
       $('.label-box').attr({id: `label-box--${id}`,for: `product_product_images_attributes_${id}_image`});
-      //選択したfileのオブジェクトを取得
       var file = this.files[0];
       var reader = new FileReader();
-      //readAsDataURLで指定したFileオブジェクトを読み込む
       reader.readAsDataURL(file);
-      //読み込み時に発火するイベント
       reader.onload = function() {
         var image = this.result;
-        //プレビューが元々なかった場合はhtmlを追加
         if ($(`#preview-box__${id}`).length == 0) {
           var count = $('.preview-box').length;
           var html = buildHTML(id);
-          //ラベルの直前のプレビュー群にプレビューを追加
           var imageContent = $('.content__product--image-label').prev();
           $(imageContent).append(html);
         }
-        //イメージを追加
+
         $(`#preview-box__${id} img`).attr('src', `${image}`);
         var count = $('.preview-box').length;
-        //プレビューが5個あったらラベルを隠す 
         if (count == 5) { 
           $('.content__product--image-label').hide();
         }
-
-        //ラベルのwidth操作
         setLabel();
-        //ラベルのidとforの値を変更
         if(count < 5){
-          //プレビューの数でラベルのオプションを更新する
           $('.label-box').attr({id: `label-box--${count}`,for: `product_product_images_attributes_${count}_image`});
         }
       }
     });
 
-    // 画像の削除
     $(document).on('click', '.delete-box', function() {
       var count = $('.preview-box').length;
       setLabel(count);
-      //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //取得したidに該当するプレビューを削除
       $(`#preview-box__${id}`).remove();
-      console.log("new")
-      //フォームの中身を削除 
       $(`#product_product_images_attributes_${id}_image`).val("");
-
-      //削除時のラベル操作
       var count = $('.preview-box').length;
-      //5個めが消されたらラベルを表示
       if (count == 4) {
         $('.content__product--image-label').show();
       }
       setLabel(count);
-
       if(id < 5){
-        //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
         $('.label-box').attr({id: `label-box--${id}`,for: `product_product_images_attributes_${id}_image`});
       }
     });
+
+
+    function appendOption(category){
+      var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
+      return html;
+    }
+
+    function buildChildrenHTML(insertHTML) {
+      var childrenHtml = `<div class="content__product--category children">
+                            <select name="product[category_id]" id="child_category">
+                              <option value="">選択してください</option>
+                              ${insertHTML}
+                            </select>
+                          </div>`;
+      $('.content__product--category.parent').append(childrenHtml);
+    }
+
+    function buildGrandchildren(insertHTML){
+      var grandchildrenHtml =`<div class="content__product--category grandchildren">
+                                <select name="product[category_id]" id="grandchild_category">
+                                  <option value="">選択してください</option>
+                                  ${insertHTML}
+                                </select>
+                              </div>`;
+      $('.content__product--category.parent').append(grandchildrenHtml);
+    }
+
+    $('#product_category_id').on('change', function(){
+      $('.children').remove(); 
+      $('.grandchildren').remove();
+      var parentCategory = document.getElementById('product_category_id').value;
+      if (!parentCategory) {
+        return
+      }
+        $.ajax({
+          url: 'get_category_children',
+          type: 'GET',
+          data: {parent_name: parentCategory},
+          dataType: 'json'
+        })
+
+        .done(function(children){
+          $('.children').remove(); 
+          $('.grandchildren').remove();
+          var insertHTML = '';
+          children.forEach(function(child){
+            insertHTML += appendOption(child);
+          });
+          buildChildrenHTML(insertHTML);
+        })
+        .fail(function(){
+          alert('カテゴリー取得に失敗しました');
+        })   
+    });
+
+    $('.parent').on('change', '#child_category', function(){
+      $('.grandchildren').remove(); 
+      var childrenCategory = $('#child_category option:selected').data('category'); 
+      if (!childrenCategory) {
+        return
+      } 
+      $.ajax({
+        url: 'get_category_grandchildren',
+        type: 'GET',
+        data: {child_id: childrenCategory},
+        dataType: 'json'
+      })
+
+      .done(function(grandchildren){
+        $('.grandchildren').remove(); 
+        var insertHTML = '';
+        grandchildren.forEach(function(grandchild){
+          insertHTML += appendOption(grandchild);
+        });
+          buildGrandchildren(insertHTML);
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    })
   });
 })
