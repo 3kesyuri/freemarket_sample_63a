b955dde3bbdfc79dc564660135910508fecb025e
3kesyuri <1989sintetsu@gmail.com>
2020-04-05 01:02:33 +0900

    turbolinksの削除

--- app/assets/javascripts/ products.js
+++ app/assets/javascripts/ products.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+
   $(function(){
 
     function buildHTML(count) {
@@ -242,4 +242,3 @@ $(document).on('turbolinks:load', function(){
       
 
     });
-});
